ca9e2491826ecbc73bdbc4d5b41b370da31a0847
Anastasia199129 <Nastasia199126@gmail.com>
2021-09-09 23:03:52 +0300

    movieDetailsPage

--- src/views/movieDetailsPage/MovieDetailsPage.jsx
+++ src/views/movieDetailsPage/MovieDetailsPage.jsx
@@ -1,10 +1,35 @@
+import axios from 'axios';
+import s from './movieDetailsPage.module.css';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const MovieDetailsPage = () => {
-  const params = useParams();
-  console.log(params);
+  const [movie, setMovie] = useState(null);
 
-  return <h1>MovieDetailsPage</h1>;
+  const { movieId } = useParams();
+  console.log(movieId);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=f63adc268da4af945e1cc9e898d72aa8`)
+      .then(response => {
+        if (response.status === 200) {
+          setMovie(response);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [movieId]);
+
+  return (
+    movie && (
+      <div>
+        <img className={s.img} src={movie.data.backdrop_path} alt="" />
+        <h2>{movie.data.original_title}</h2>
+      </div>
+    )
+  );
 };
 
 export default MovieDetailsPage;
